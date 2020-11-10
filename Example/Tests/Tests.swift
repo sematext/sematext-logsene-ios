18fe84f1db104b033b829d607b1f0e0e1380e81b
Rafał Kuć <r.kuc@solr.pl>
2020-11-10 10:39:59 +0100

    Missing file

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,10 +1,22 @@
-// https://github.com/Quick/Quick
-
-import Quick
-import Nimble
 import Logsene
+import XCTest
 
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
+class LogseneTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+    
+    func testLogsene() {
+        try! LogseneInit("du72esds-s21s-xws2-2sww-sas1dwsa12sa", type: "example")
+        LLogInfo("Sample message")
+        XCTAssertTrue(true)
     }
 }
